6cfa4b6a23682fd4dd0af0585db85fdacd40f7ff
PICHET ARJJINDA <jinda789amulet@gmail.com>
2024-01-07 10:24:49 +0700

    Update target_list.js

--- target_list.js
+++ target_list.js
@@ -1,6 +1,7 @@
 const target = [
     "0x6CA298D2983aB03Aa1dA7679389D955A4eFEE15C", 
     "0x3c9ad6a3ff46bbc218e58801876667dc68e9eda2",
+    "0x95222290dd7278aa3ddd389cc1e1d165cc4bafe5"
 ]
 
 module.exports = target
